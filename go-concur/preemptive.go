bc0e28771ca65e6d3ed15a06895ea4d6b5075b2a
Longfei Xing <longfeix@uber.com>
2020-01-12 02:54:48 -0500

    meetup

---
+++ go-concur/preemptive.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "runtime"
+import "time"
+
+func cpuIntensive(p *int) {
+	for i := 1; i <= 100000000000; i++ {
+		*p = i
+		if i % 100000 == 0 {
+			// uncomment the below to un-hang
+			//println(i)
+		}
+	}
+}
+
+func preemptive() {
+	runtime.GOMAXPROCS(1)
+
+	x := 0
+	go cpuIntensive(&x)
+
+	time.Sleep(100 * time.Millisecond)
+
+	// printed only after cpuIntensive is completely finished
+	fmt.Printf("x = %d.\n", x)
+}
